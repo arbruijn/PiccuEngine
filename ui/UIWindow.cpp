aa14d9e002c37f4d0be00cbbc67d340c5c7ce79b
Arne de Bruijn <mail@arnedebruijn.nl>
2026-04-27 17:50:05 +0200

    Fix gcc error taking address of rvalue
    Passing a temporary object to a non-const reference is not allowed.
    
    Work around it with a wrapper function that takes a const reference
    and casts the const away.

--- ui/UIWindow.cpp
+++ ui/UIWindow.cpp
@@ -790,7 +790,8 @@ void UITitledWindow::Create(UITextItem &title, int x, int y, int w, int h)
 	m_CaptionColor = GR_DARKGRAY;
 	
 	UIWindow::Create(x,y,w,h);
-	UIWindow::SetBackItem(&UIPrimativeItem(GR_LIGHTGRAY));
+	UIPrimativeItem back_item(GR_LIGHTGRAY);
+	UIWindow::SetBackItem(&back_item);
 }
 
 
